38cc7684bbbea781d21af7cee6f14883c63b784f
KitPyles <pyles.kit@gmail.com>
2023-02-22 14:33:55 -0600

    Added SSC dependency

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -15,9 +15,9 @@ import java.util.List;
 public class StudentController {
 
     private static final List<Student> STUDENTS = Arrays.asList(
-        new Student(1, "James Bond"),
-        new Student(2, "Maria Jones"),
-        new Student(3, "Anna Smith")
+      new Student(1, "James Bond"),
+      new Student(2, "Maria Jones"),
+      new Student(3, "Anna Smith")
     );
 
     @GetMapping(path = "{studentId}")
@@ -25,6 +25,8 @@ public class StudentController {
         return STUDENTS.stream()
                 .filter(student -> studentId.equals(student.getStudentId()))
                 .findFirst()
-                .orElseThrow(() -> new IllegalStateException("Student " + studentId +" does not exist."));
+                .orElseThrow(() -> new IllegalStateException(
+                        "Student " + studentId + " does not exists"
+                ));
     }
 }
